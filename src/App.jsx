8b6c93d852fa9f1f0d1012b5da23aaa61384747c
njenkins2727 <njenkins2727@gmail.com>
2025-01-22 08:14:37 +0800

    setup application, have retrieved up account name and balance for future display

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import AccountName from './UpAccount'
+import './App.css'
+function App() {
+
+  return (
+    <div>
+      <button onClick={AccountName}>click for info</button>
+    </div>
+  )
+}
+
+export default App
