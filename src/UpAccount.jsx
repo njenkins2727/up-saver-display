4ca38fc5520bc4b074bb3b9d9c8cc32bc17fda2b
njenkins2727 <njenkins2727@gmail.com>
2025-05-02 10:35:58 +0800

    shows correct date in last updated. when the last transaction occured in this account

--- src/UpAccount.jsx
+++ src/UpAccount.jsx
@@ -26,13 +26,33 @@ useEffect(() => {
       //Using useState to set response from our fetch into a variable we can easily refer back to  
       setName(results.data[1].attributes.displayName); 
       setBalance(results.data[1].attributes.balance.value);
-      setTimestamp(results.data[1].attributes.createdAt);
 
     }catch(error){
       console.log('failed to fetch account info', error) 
     }
   })
   getInfo();
+
+  const getTransaction = (async () => {
+    try{
+
+      const response = await fetch('https://api.up.com.au/api/v1/accounts/ecd703f5-2bcd-4677-aed0-2f367a3e67a7/transactions', { 
+        method: "GET",
+        headers: {'Authorization': `Bearer ${import.meta.env.VITE_UP_API_KEY}`}
+      })
+
+      if (!response.ok) { //If the response is invalid then return an error with the status and message
+        throw new Error(`Transaction Info Error: ${response.status} ${response.statusText}`);
+      }
+
+      const results = await response.json(); // Since it is an asynchronus function we must await the resonse and convert into json for readability  
+        setTimestamp(results.data[0].attributes.createdAt)
+
+    }catch(error){
+      console.log('failed to fetch Transaction info', error) 
+    }
+  })
+  getTransaction();
 
   const handleUpdate = () => {
     getInfo(); // re-fetch name and balance 
